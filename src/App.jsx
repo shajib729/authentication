c64ce7d0de09f993b55f504307a6767a809f2644
Shajib <sajidislam729@gmail.com>
2021-12-23 21:11:13 +0600

    front end

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import Navbar from "./components/Navbar";
+import "./app.css";
+import Home from "./pages/Home";
+import Post from "./pages/Post";
+import Login from "./pages/Login";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+const App = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const getUser =async () => {
+      const res=await fetch("http://localhost:5000/auth/login/success", {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Credentials": true,
+        },
+      })
+      if (res.status === 200) {
+        const data = await res.json()
+        setUser(data.user)
+      }
+      else {
+        throw new Error("Authentication has been failed")
+      }
+    };
+    getUser();
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <div>
+        <Navbar user={user} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/login"
+            element={user ? <Navigate to="/" /> : <Login />}
+          />
+          <Route
+            path="/post/:id"
+            element={user ? <Post /> : <Navigate to="/login" />}
+          />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
